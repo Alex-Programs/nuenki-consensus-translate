2eeba25787964ddd0fe8bf996bed56e14040abb4
Alex-Programs <alexcj2802@gmail.com>
2025-04-23 18:54:20 +0100

    fix copy-paste mistake

--- consensus-translate/src/get_source.rs
+++ consensus-translate/src/get_source.rs
@@ -2,7 +2,7 @@ use crate::{languages::Language, TranslationSource};
 
 const GPT4O: &'static str = "openai/gpt-4o-2024-11-20";
 const GPT41: &'static str = "openai/gpt-4.1";
-const GEMINI_FLASH2_5: &'static str = "google/google/gemini-2.5-flash-preview";
+const GEMINI_FLASH2_5: &'static str = "google/gemini-2.5-flash-preview";
 const LLAMA33_70B: &'static str = "meta-llama/llama-3.3-70b-instruct";
 const SONNET35: &'static str = "anthropic/claude-3.5-sonnet";
 const GEMMA3_27B: &'static str = "google/gemma-3-27b-it";
